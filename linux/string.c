cd6317fba1990d641414ca7e486f239e33709f0d
Colby Skeggs <skeggsc@catlin.edu>
2015-05-14 13:03:00 -0700

    Added rudimentary implementation of string.c and fixed up Makefile.

---
+++ linux/string.c
@@ -0,0 +1,44 @@
+#include <string.h>
+
+ulen strlen(string str) {
+	ulen out = 0;
+	while (str[out]) {
+		out++;
+	}
+	return out;
+}
+ulen strnlen(string str, ulen max) {
+	ulen out = 0;
+	while (out < max && str[out]) {
+		out++;
+	}
+	return out;
+}
+
+/* TODO
+mutable_string strmov(mutable_string restrict dest, ulen max, string restrict src);
+mutable_string strapnd(mutable_string restrict dest, ulen max, string restrict src);
+i16 strcmp(string lhs, string rhs);
+bool streq(string lhs, string rhs);
+string strchr(string str, u8 chr);
+string strrchr(string str, u8 chr);
+string strstr(string str, string substr);
+mutable_string strdup(string str);
+
+typedef struct {
+	ulen count;
+	string *strings;
+} strsplit_out;
+strsplit_out *strsplit(string str, string delimiter, bool compress);
+void strsplit_free(strsplit_out *struc);
+
+// if remain is NULL, then will panic if invalid. otherwise, will be set to point to the first unconsumed character.
+u8 parseu8(string data, string *remain, u8 base);
+u16 parseu16(string data, string *remain, u8 base);
+u32 parseu32(string data, string *remain, u8 base);
+u64 parseu64(string data, string *remain, u8 base);
+i8 parsei8(string data, string *remain, u8 base);
+i16 parsei16(string data, string *remain, u8 base);
+i32 parsei32(string data, string *remain, u8 base);
+i64 parsei64(string data, string *remain, u8 base);
+*/
