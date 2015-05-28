5d0efe56fb4eb6b4d48c3035473ff7f7c4a88ca8
Colby Skeggs <skeggsc@catlin.edu>
2015-05-28 09:09:14 -0700

    Updated licensing to be less complicated.

--- include/clock.h
+++ include/clock.h
@@ -1,8 +1,21 @@
 /*
- * Copyright (c) 2015 Colby Skeggs
- * This file is part of libca, and is licensed under the MIT license.
- * See LICENSE.txt for details.
- */
+    Copyright 2015 Colby Skeggs
+
+    This file is part of libca. See LICENSE.txt for details.
+
+    libca is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libca is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with libca.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _CLOCK_H
 #define _CLOCK_H
--- linux/file.c
+++ linux/file.c
@@ -1,18 +1,20 @@
 /*
-    This file is part of libca/linux. See LICENSE.txt for details.
+    Copyright 2015 Colby Skeggs
 
-    libca/linux is free software: you can redistribute it and/or modify
+    This file is part of libca. See LICENSE.txt for details.
+
+    libca is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    libca/linux is distributed in the hope that it will be useful,
+    libca is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with libca/linux.  If not, see <http://www.gnu.org/licenses/>.
+    along with libca.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <assert.h>
