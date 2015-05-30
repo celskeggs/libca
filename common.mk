d190bb6f627eff9182dd9b0601341a9df68c472f
Colby Skeggs <skeggsc@catlin.edu>
2015-05-30 15:11:42 -0700

    Possibly fixed echo issue on Travis.

--- common.mk
+++ common.mk
@@ -5,4 +5,4 @@ CC=gcc
 CFLAGS=-O2 -ggdb -Wall
 ALL_CFLAGS=-m$(BITS) -nostdlib -nostdinc -std=c99 -I$(INCDIR) -I$(SRCDIR)/libsyscall-built -fno-builtin $(CFLAGS)
 
-echo=echo -e "\e[1;3$(1)m[$(2)] $(3)\e[0m"
+echo=/bin/echo -e "\e[1;3$(1)m[$(2)] $(3)\e[0m"
